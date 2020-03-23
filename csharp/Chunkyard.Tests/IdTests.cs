ddb5e3204efbe699500dcdc13a9d03002de6fb49
Florian Winkelbauer <mail@florianwinkelbauer.com>
2020-03-23 16:14:50 +0100

    Run code cleanup

--- csharp/Chunkyard.Tests/IdTests.cs
+++ csharp/Chunkyard.Tests/IdTests.cs
@@ -60,8 +60,8 @@ namespace Chunkyard.Tests
             Assert.Equal(expectedUri, actualUri);
         }
 
-         [Fact]
-         public static void FromContentUri_Can_Split_ContentUri()
+        [Fact]
+        public static void FromContentUri_Can_Split_ContentUri()
         {
             var contentUri = new Uri("sha256://ad95131bc0b799c0b1af477fb14fcf26a6a9f76079e48bf090acb7e8367bfd0e");
 
