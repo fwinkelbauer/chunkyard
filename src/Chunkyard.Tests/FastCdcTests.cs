93b267139adc9128de0d23c02aff626a7a6e7295
Florian Winkelbauer <mail@florianwinkelbauer.com>
2020-05-22 08:35:28 +0200

    Port remaining FastCdc tests

--- src/Chunkyard.Tests/FastCdcTests.cs
+++ src/Chunkyard.Tests/FastCdcTests.cs
@@ -1,9 +1,64 @@
-﻿using Xunit;
+﻿using System.IO;
+using System.Linq;
+using Xunit;
 
 namespace Chunkyard.Tests
 {
     public static class FastCdcTests
     {
+        [Fact]
+        public static void SplitIntoChunks_Sekien_16k_Chunks()
+        {
+            var fastCdc = new FastCdc(
+                8 * 1024,
+                16 * 1024,
+                32 * 1024);
+
+            using var stream = File.OpenRead("SekienAkashita.jpg");
+            var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
+
+            Assert.Equal(6, chunks.Length);
+            Assert.Equal(22366, chunks[0].Length);
+            Assert.Equal(8282, chunks[1].Length);
+            Assert.Equal(16303, chunks[2].Length);
+            Assert.Equal(18696, chunks[3].Length);
+            Assert.Equal(32768, chunks[4].Length);
+            Assert.Equal(11051, chunks[5].Length);
+        }
+
+        [Fact]
+        public static void SplitIntoChunks_Sekien_32k_Chunks()
+        {
+            var fastCdc = new FastCdc(
+                16 * 1024,
+                32 * 1024,
+                64 * 1024);
+
+            using var stream = File.OpenRead("SekienAkashita.jpg");
+            var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
+
+            Assert.Equal(3, chunks.Length);
+            Assert.Equal(32857, chunks[0].Length);
+            Assert.Equal(16408, chunks[1].Length);
+            Assert.Equal(60201, chunks[2].Length);
+        }
+
+        [Fact]
+        public static void SplitIntoChunks_Sekien_64k_Chunks()
+        {
+            var fastCdc = new FastCdc(
+                32 * 1024,
+                64 * 1024,
+                128 * 1024);
+
+            using var stream = File.OpenRead("SekienAkashita.jpg");
+            var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
+
+            Assert.Equal(2, chunks.Length);
+            Assert.Equal(32857, chunks[0].Length);
+            Assert.Equal(76609, chunks[1].Length);
+        }
+
         [Theory]
         [InlineData(65537, 16)]
         [InlineData(65536, 16)]
@@ -11,13 +66,13 @@ namespace Chunkyard.Tests
         [InlineData(32769, 15)]
         [InlineData(32768, 15)]
         [InlineData(32767, 15)]
-        public static void Test_Logarithm2(int inputValue, int expectedValue)
+        public static void Logarithm2_Test(int inputValue, int expectedValue)
         {
             Assert.Equal(expectedValue, FastCdc.Logarithm2(inputValue));
         }
 
         [Fact]
-        public static void Test_Logarithm2_Assumptions()
+        public static void Logarithm2_Test_Assumptions()
         {
             Assert.True(FastCdc.Logarithm2(FastCdc.AverageMin) >= 8);
             Assert.True(FastCdc.Logarithm2(FastCdc.AverageMax) >= 28);
@@ -30,7 +85,7 @@ namespace Chunkyard.Tests
         [InlineData(9, 3, 3)]
         [InlineData(6, 2, 3)]
         [InlineData(5, 2, 3)]
-        public static void Test_CeilDiv(int x, int y, int expectedValue)
+        public static void CeilDiv_Test(int x, int y, int expectedValue)
         {
             Assert.Equal(expectedValue, FastCdc.CeilDiv(x, y));
         }
@@ -39,7 +94,7 @@ namespace Chunkyard.Tests
         [InlineData(50, 100, 50, 0)]
         [InlineData(200, 100, 50, 50)]
         [InlineData(200, 100, 40, 40)]
-        public static void Test_CenterSize(
+        public static void CenterSize_Test(
             int average,
             int minimum,
             int sourceSize,
@@ -55,7 +110,7 @@ namespace Chunkyard.Tests
         [InlineData(16, 65535)]
         [InlineData(10, 1023)]
         [InlineData(8, 255)]
-        public static void Test_Mask(int bits, uint expectedValue)
+        public static void Mask_Test(int bits, uint expectedValue)
         {
             Assert.Equal(expectedValue, FastCdc.Mask(bits));
         }
