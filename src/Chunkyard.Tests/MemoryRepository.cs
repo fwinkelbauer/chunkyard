b326485dc68e9e096510e353d1c07eee07c2135a
Florian Winkelbauer <mail@florianwinkelbauer.com>
2020-11-12 15:19:50 +0100

    Remove locks from MemoryRepository

--- src/Chunkyard.Tests/MemoryRepository.cs
+++ src/Chunkyard.Tests/MemoryRepository.cs
@@ -6,13 +6,11 @@ namespace Chunkyard.Tests
 {
     public class MemoryRepository : IRepository
     {
-        private readonly object _lock;
         private readonly Dictionary<Uri, byte[]> _valuesByUri;
         private readonly Dictionary<int, byte[]> _valuesByLog;
 
         public MemoryRepository(Guid? repositoryId = null)
         {
-            _lock = new object();
             _valuesByUri = new Dictionary<Uri, byte[]>();
             _valuesByLog = new Dictionary<int, byte[]>();
 
@@ -26,85 +24,58 @@ namespace Chunkyard.Tests
 
         public bool StoreValue(Uri contentUri, byte[] value)
         {
-            lock (_lock)
+            if (_valuesByUri.ContainsKey(contentUri))
             {
-                if (_valuesByUri.ContainsKey(contentUri))
-                {
-                    return false;
-                }
-
-                _valuesByUri[contentUri] = value;
-
-                return true;
+                return false;
             }
+
+            _valuesByUri[contentUri] = value;
+
+            return true;
         }
 
         public byte[] RetrieveValue(Uri contentUri)
         {
-            lock (_lock)
-            {
-                return _valuesByUri[contentUri];
-            }
+            return _valuesByUri[contentUri];
         }
 
         public bool ValueExists(Uri contentUri)
         {
-            lock (_lock)
-            {
-                return _valuesByUri.ContainsKey(contentUri);
-            }
+            return _valuesByUri.ContainsKey(contentUri);
         }
 
         public Uri[] ListUris()
         {
-            lock (_lock)
-            {
-                return _valuesByUri.Keys
-                    .OrderByDescending(uri => uri.AbsoluteUri)
-                    .ToArray();
-            }
+            return _valuesByUri.Keys
+                .OrderByDescending(uri => uri.AbsoluteUri)
+                .ToArray();
         }
 
         public void RemoveValue(Uri contentUri)
         {
-            lock (_lock)
-            {
-                _valuesByUri.Remove(contentUri);
-            }
+            _valuesByUri.Remove(contentUri);
         }
 
         public int AppendToLog(int newLogPosition, byte[] value)
         {
-            lock (_lock)
-            {
-                _valuesByLog.Add(newLogPosition, value);
+            _valuesByLog.Add(newLogPosition, value);
 
-                return newLogPosition;
-            }
+            return newLogPosition;
         }
 
         public byte[] RetrieveFromLog(int logPosition)
         {
-            lock (_lock)
-            {
-                return _valuesByLog[logPosition];
-            }
+            return _valuesByLog[logPosition];
         }
 
         public void RemoveFromLog(int logPosition)
         {
-            lock (_lock)
-            {
-                _valuesByLog.Remove(logPosition);
-            }
+            _valuesByLog.Remove(logPosition);
         }
 
         public int[] ListLogPositions()
         {
-            lock (_lock)
-            {
-                return _valuesByLog.Keys.ToArray();
-            }
+            return _valuesByLog.Keys.ToArray();
         }
     }
 }
