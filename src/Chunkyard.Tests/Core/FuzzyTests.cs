a3134ca1c1b3a0f546db6929657653aa2264c73f
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-05-17 15:52:07 +0200

    Improve FuzzyTests readability

--- src/Chunkyard.Tests/Core/FuzzyTests.cs
+++ src/Chunkyard.Tests/Core/FuzzyTests.cs
@@ -7,27 +7,29 @@ namespace Chunkyard.Tests.Core
     public static class FuzzyTests
     {
         [Fact]
-        public static void Empty_Pattern_Matches_Everything()
+        public static void Empty_String_Matches_Everything()
         {
-            var fuzzy1 = Fuzzy.MatchAll;
-            var fuzzy2 = new Fuzzy(new[] { "" }, emptyMatches: true);
-            var fuzzy3 = new Fuzzy(new[] { "" }, emptyMatches: false);
+            var fuzzy1 = new Fuzzy(new[] { "" }, emptyMatches: true);
+            var fuzzy2 = new Fuzzy(new[] { "" }, emptyMatches: false);
 
             Assert.True(fuzzy1.IsMatch("some text!"));
             Assert.True(fuzzy2.IsMatch("some text!"));
-            Assert.True(fuzzy3.IsMatch("some text!"));
         }
 
         [Fact]
-        public static void No_Parameter_Matches_Maybe()
+        public static void Empty_Collection_Matches_Maybe()
         {
-            var fuzzy1 = new Fuzzy(Array.Empty<string>(), emptyMatches: true);
-            var fuzzy2 = new Fuzzy(Array.Empty<string>(), emptyMatches: false);
+            var fuzzy1 = Fuzzy.MatchAll;
+            var fuzzy2 = new Fuzzy(Array.Empty<string>(), emptyMatches: true);
+
             var fuzzy3 = Fuzzy.MatchNothing;
+            var fuzzy4 = new Fuzzy(Array.Empty<string>(), emptyMatches: false);
 
             Assert.True(fuzzy1.IsMatch("some text!"));
-            Assert.False(fuzzy2.IsMatch("some text!"));
+            Assert.True(fuzzy2.IsMatch("some text!"));
+
             Assert.False(fuzzy3.IsMatch("some text!"));
+            Assert.False(fuzzy4.IsMatch("some text!"));
         }
 
         [Fact]
@@ -40,6 +42,7 @@ namespace Chunkyard.Tests.Core
             Assert.True(fuzzy.IsMatch("Hello World!"));
             Assert.True(fuzzy.IsMatch("Held"));
             Assert.True(fuzzy.IsMatch("HELLO WORLD!"));
+
             Assert.False(fuzzy.IsMatch("hello world!"));
             Assert.False(fuzzy.IsMatch("Goodbye World!"));
         }
