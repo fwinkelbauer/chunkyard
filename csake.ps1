42f59f0ccef55761fc53122425715ba2714c6bb8
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-09-27 07:35:00 +0200

    Improve csake
    This way the script can be used anywhere in the project. The script can
    now be copied to a global place like ~/bin

--- csake.ps1
+++ csake.ps1
@@ -1,3 +1,15 @@
-dotnet run --project 'src/Chunkyard.Build' -- $args
+$root = git rev-parse --show-toplevel
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-exit $LASTEXITCODE
+try {
+    Push-Location $root
+
+    $project = git ls-files '*.Build.csproj'
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    dotnet run --project $project -- $args
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+}
+finally {
+    Pop-Location
+}
