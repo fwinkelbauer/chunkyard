1dafb93f1678b5c7e632f6309e095e62ea0619dd
Florian Winkelbauer <mail@florianwinkelbauer.com>
2021-12-12 21:29:02 +0100

    Simplify FastCdcTests
    By introducing a helper method to reduce code duplication

--- src/Chunkyard.Tests/Core/FastCdcTests.cs
+++ src/Chunkyard.Tests/Core/FastCdcTests.cs
@@ -8,56 +8,43 @@ public static class FastCdcTests
     [Fact]
     public static void SplitIntoChunks_16k_Chunks()
     {
-        var fastCdc = new FastCdc(
-            8 * 1024,
-            16 * 1024,
-            32 * 1024);
-
-        using var stream = new MemoryStream(_pictureBytes);
-        var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
-
-        Assert.Equal(
-            new[] { 22366, 8282, 16303, 18696, 32768, 11051 },
-            chunks.Select(c => c.Length));
-
-        Assert.Equal(
-            _pictureBytes,
-            chunks.SelectMany(c => c));
+        AssertSplit(
+            new FastCdc(
+                8 * 1024,
+                16 * 1024,
+                32 * 1024),
+            new[] { 22366, 8282, 16303, 18696, 32768, 11051 });
     }
 
     [Fact]
     public static void SplitIntoChunks_32k_Chunks()
     {
-        var fastCdc = new FastCdc(
-            16 * 1024,
-            32 * 1024,
-            64 * 1024);
-
-        using var stream = new MemoryStream(_pictureBytes);
-        var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
-
-        Assert.Equal(
-            new[] { 32857, 16408, 60201 },
-            chunks.Select(c => c.Length));
-
-        Assert.Equal(
-            _pictureBytes,
-            chunks.SelectMany(c => c));
+        AssertSplit(
+            new FastCdc(
+                16 * 1024,
+                32 * 1024,
+                64 * 1024),
+            new[] { 32857, 16408, 60201 });
     }
 
     [Fact]
     public static void SplitIntoChunks_64k_Chunks()
     {
-        var fastCdc = new FastCdc(
-            32 * 1024,
-            64 * 1024,
-            128 * 1024);
+        AssertSplit(
+            new FastCdc(
+                32 * 1024,
+                64 * 1024,
+                128 * 1024),
+            new[] { 32857, 76609 });
+    }
 
+    private static void AssertSplit(FastCdc fastCdc, int[] chunkSizes)
+    {
         using var stream = new MemoryStream(_pictureBytes);
         var chunks = fastCdc.SplitIntoChunks(stream).ToArray();
 
         Assert.Equal(
-            new[] { 32857, 76609 },
+            chunkSizes,
             chunks.Select(c => c.Length));
 
         Assert.Equal(
