8d0cab339ebf4bb7c921df90be55f6650b4ab9bf
Florian Winkelbauer <mail@florianwinkelbauer.com>
2022-03-18 16:50:11 +0100

    Simplify csake scripts

--- csake.ps1
+++ csake.ps1
@@ -1,15 +1,2 @@
-$root = git rev-parse --show-toplevel
+dotnet run --project "src/Chunkyard.Build/Chunkyard.Build.csproj" -- $args
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-try {
-    Push-Location $root
-
-    $project = git ls-files '*.Build.csproj'
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-    dotnet run --project $project -- $args
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-}
-finally {
-    Pop-Location
-}
