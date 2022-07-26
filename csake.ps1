201466a0f84cb77be83934891a5966123d969d66
Florian Winkelbauer <mail@florianwinkelbauer.com>
2022-07-26 15:03:29 +0200

    Update csake scripts

--- csake.ps1
+++ csake.ps1
@@ -1,2 +1,2 @@
-dotnet run --project "src/Chunkyard.Build/Chunkyard.Build.csproj" -- $args
+dotnet run --project 'src/Chunkyard.Build/Chunkyard.Build.csproj' -- $args
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
