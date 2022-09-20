8d7d997e3e4a3f4ebd0a2742de39713c9e5157b4
Florian Winkelbauer <mail@florianwinkelbauer.com>
2022-09-20 20:46:13 +0200

    Revert "Add new RingBuffer.cs"
    This reverts commit 917a40fc81eac8961370a4407468fa1a0ef56ad3.

--- src/Chunkyard/Usings.cs
+++ src/Chunkyard/Usings.cs
@@ -14,6 +14,5 @@ global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Text.RegularExpressions;
-global using System.Threading;
 
 [assembly: CLSCompliant(false)]
