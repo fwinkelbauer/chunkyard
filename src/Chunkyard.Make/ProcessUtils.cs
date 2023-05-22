6daf687c5b8d4cf1dbfd774489c5da5c8c3dfe25
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-05-22 18:33:08 +0200

    Replace validExitCodes with a function

--- src/Chunkyard.Make/ProcessUtils.cs
+++ src/Chunkyard.Make/ProcessUtils.cs
@@ -7,7 +7,7 @@ public static class ProcessUtils
 {
     public static void Run(
         ProcessStartInfo startInfo,
-        int[]? validExitCodes = null,
+        Func<int, bool>? isValidExitCodes = null,
         Action<string>? processOutput = null)
     {
         using var process = Process.Start(startInfo);
@@ -40,9 +40,9 @@ public static class ProcessUtils
 
         process.WaitForExit();
 
-        validExitCodes ??= new[] { 0 };
+        isValidExitCodes ??= exitCode => exitCode == 0;
 
-        if (!validExitCodes.Contains(process.ExitCode))
+        if (!isValidExitCodes(process.ExitCode))
         {
             throw new InvalidOperationException(
                 $"Exit code of '{startInfo.FileName}' was {process.ExitCode}");
@@ -52,20 +52,20 @@ public static class ProcessUtils
     public static void Run(
         string fileName,
         string arguments,
-        int[]? validExitCodes = null)
+        Func<int, bool>? isValidExitCodes = null)
     {
-        Run(new ProcessStartInfo(fileName, arguments), validExitCodes);
+        Run(new ProcessStartInfo(fileName, arguments), isValidExitCodes);
     }
 
     public static string RunQuery(
         ProcessStartInfo startInfo,
-        int[]? validExitCodes = null)
+        Func<int, bool>? isValidExitCodes = null)
     {
         var builder = new StringBuilder();
 
         Run(
             startInfo,
-            validExitCodes,
+            isValidExitCodes,
             line => builder.AppendLine(line));
 
         return builder.ToString().TrimEnd();
@@ -74,13 +74,13 @@ public static class ProcessUtils
     public static string RunQuery(
         string fileName,
         string arguments,
-        int[]? validExitCodes = null)
+        Func<int, bool>? isValidExitCodes = null)
     {
         return RunQuery(
             new ProcessStartInfo(fileName, arguments)
             {
                 RedirectStandardOutput = true
             },
-            validExitCodes);
+            isValidExitCodes);
     }
 }
