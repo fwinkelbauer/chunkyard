b0da93148afbcb31b39cf8836535c570539047a2
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-05-23 16:45:38 +0200

    Fix typo in parameter names

--- src/Chunkyard.Make/ProcessUtils.cs
+++ src/Chunkyard.Make/ProcessUtils.cs
@@ -7,7 +7,7 @@ public static class ProcessUtils
 {
     public static void Run(
         ProcessStartInfo startInfo,
-        Func<int, bool>? isValidExitCodes = null,
+        Func<int, bool>? isValidExitCode = null,
         Action<string>? processOutput = null)
     {
         using var process = Process.Start(startInfo);
@@ -40,9 +40,9 @@ public static class ProcessUtils
 
         process.WaitForExit();
 
-        isValidExitCodes ??= exitCode => exitCode == 0;
+        isValidExitCode ??= exitCode => exitCode == 0;
 
-        if (!isValidExitCodes(process.ExitCode))
+        if (!isValidExitCode(process.ExitCode))
         {
             throw new InvalidOperationException(
                 $"Exit code of '{startInfo.FileName}' was {process.ExitCode}");
@@ -52,20 +52,20 @@ public static class ProcessUtils
     public static void Run(
         string fileName,
         string arguments,
-        Func<int, bool>? isValidExitCodes = null)
+        Func<int, bool>? isValidExitCode = null)
     {
-        Run(new ProcessStartInfo(fileName, arguments), isValidExitCodes);
+        Run(new ProcessStartInfo(fileName, arguments), isValidExitCode);
     }
 
     public static string RunQuery(
         ProcessStartInfo startInfo,
-        Func<int, bool>? isValidExitCodes = null)
+        Func<int, bool>? isValidExitCode = null)
     {
         var builder = new StringBuilder();
 
         Run(
             startInfo,
-            isValidExitCodes,
+            isValidExitCode,
             line => builder.AppendLine(line));
 
         return builder.ToString().TrimEnd();
@@ -74,13 +74,13 @@ public static class ProcessUtils
     public static string RunQuery(
         string fileName,
         string arguments,
-        Func<int, bool>? isValidExitCodes = null)
+        Func<int, bool>? isValidExitCode = null)
     {
         return RunQuery(
             new ProcessStartInfo(fileName, arguments)
             {
                 RedirectStandardOutput = true
             },
-            isValidExitCodes);
+            isValidExitCode);
     }
 }
