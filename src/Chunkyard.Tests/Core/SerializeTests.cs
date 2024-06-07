a9b746f7ea0a2a1d58e9d08b80b0009e7444a198
Florian Winkelbauer <mail@florianwinkelbauer.com>
2024-06-07 14:39:33 +0200

    Add tests to keep serialized schema

--- src/Chunkyard.Tests/Core/SerializeTests.cs
+++ src/Chunkyard.Tests/Core/SerializeTests.cs
@@ -33,4 +33,70 @@ public static class SerializeTests
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public static void Serialize_Respects_Serialized_Snapshot()
+    {
+        var date = DateTime.Parse("2024-06-07T12:06:43.6536137Z")
+            .ToUniversalTime();
+
+        var expected = new Snapshot(
+            date,
+            new[]
+            {
+                new BlobReference(
+                    new Blob("some blob", date),
+                    new[] { "ad95131bc0b799c0b1af477fb14fcf26a6a9f76079e48bf090acb7e8367bfd0e" })
+            });
+
+        var json = """
+            {
+              "CreationTimeUtc": "2024-06-07T12:06:43.6536137Z",
+              "BlobReferences": [
+                {
+                  "Blob": {
+                    "Name": "some blob",
+                    "LastWriteTimeUtc": "2024-06-07T12:06:43.6536137Z"
+                  },
+                  "ChunkIds": [
+                    "ad95131bc0b799c0b1af477fb14fcf26a6a9f76079e48bf090acb7e8367bfd0e"
+                  ]
+                }
+              ]
+            }
+            """;
+
+        var actual = Serialize.BytesToSnapshot(
+            Encoding.UTF8.GetBytes(json));
+
+        Assert.True(
+            expected.Equals(actual),
+            "Backups of previous Chunkyard versions cannot be read");
+    }
+
+    [Fact]
+    public static void Serialize_Respects_Serialized_SnapshotReference()
+    {
+        var expected = new SnapshotReference(
+            new byte[] { 158, 128, 181, 90, 139, 201, 73, 163, 30, 55, 127, 23 },
+            100000,
+            new[] { "ad95131bc0b799c0b1af477fb14fcf26a6a9f76079e48bf090acb7e8367bfd0e" });
+
+        var json = """
+            {
+              "Salt": "noC1WovJSaMeN38X",
+              "Iterations": 100000,
+              "ChunkIds": [
+                "ad95131bc0b799c0b1af477fb14fcf26a6a9f76079e48bf090acb7e8367bfd0e"
+              ]
+            }
+            """;
+
+        var actual = Serialize.BytesToSnapshotReference(
+            Encoding.UTF8.GetBytes(json));
+
+        Assert.True(
+            expected.Equals(actual),
+            "Backups of previous Chunkyard versions cannot be read");
+    }
 }
