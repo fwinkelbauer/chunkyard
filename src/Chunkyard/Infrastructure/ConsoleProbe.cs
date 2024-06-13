0a68c28184e9b7e3ece6e0903e679a1e1f5b680a
Florian Winkelbauer <mail@florianwinkelbauer.com>
2024-06-13 19:04:55 +0200

    Let ConsoleProbe write to Console.Error

--- src/Chunkyard/Infrastructure/ConsoleProbe.cs
+++ src/Chunkyard/Infrastructure/ConsoleProbe.cs
@@ -7,54 +7,54 @@ internal sealed class ConsoleProbe : IProbe
 {
     public void StoredBlob(Blob blob)
     {
-        Console.WriteLine($"Stored blob: {blob.Name}");
+        Console.Error.WriteLine($"Stored blob: {blob.Name}");
     }
 
     public void RestoredBlob(Blob blob)
     {
-        Console.WriteLine($"Restored blob: {blob.Name}");
+        Console.Error.WriteLine($"Restored blob: {blob.Name}");
     }
 
     public void BlobValid(Blob blob, bool valid)
     {
-        Console.WriteLine(valid
+        Console.Error.WriteLine(valid
             ? $"Valid blob: {blob.Name}"
             : $"Invalid blob: {blob.Name}");
     }
 
     public void CopiedChunk(string chunkId)
     {
-        Console.WriteLine($"Copied chunk: {chunkId}");
+        Console.Error.WriteLine($"Copied chunk: {chunkId}");
     }
 
     public void RemovedChunk(string chunkId)
     {
-        Console.WriteLine($"Removed chunk: {chunkId}");
+        Console.Error.WriteLine($"Removed chunk: {chunkId}");
     }
 
     public void CopiedSnapshot(int snapshotId)
     {
-        Console.WriteLine($"Copied snapshot: #{snapshotId}");
+        Console.Error.WriteLine($"Copied snapshot: #{snapshotId}");
     }
 
     public void StoredSnapshot(int snapshotId)
     {
-        Console.WriteLine($"Stored snapshot: #{snapshotId}");
+        Console.Error.WriteLine($"Stored snapshot: #{snapshotId}");
     }
 
     public void RestoredSnapshot(int snapshotId)
     {
-        Console.WriteLine($"Restored snapshot: #{snapshotId}");
+        Console.Error.WriteLine($"Restored snapshot: #{snapshotId}");
     }
 
     public void RemovedSnapshot(int snapshotId)
     {
-        Console.WriteLine($"Removed snapshot: #{snapshotId}");
+        Console.Error.WriteLine($"Removed snapshot: #{snapshotId}");
     }
 
     public void SnapshotValid(int snapshotId, bool valid)
     {
-        Console.WriteLine(valid
+        Console.Error.WriteLine(valid
             ? $"Valid snapshot: #{snapshotId}"
             : $"Invalid snapshot: #{snapshotId}");
     }
