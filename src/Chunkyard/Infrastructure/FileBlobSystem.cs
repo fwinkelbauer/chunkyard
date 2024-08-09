f1ab1e2cbfc9855471b218db0dc346b90efa6b46
Florian Winkelbauer <mail@florianwinkelbauer.com>
2024-08-09 15:24:58 +0200

    Revert "Add exception when a blob does not exist"
    This reverts commit 81ac1f9e04bd58d0fb4eaa0002e47abd0449342c.

--- src/Chunkyard/Infrastructure/FileBlobSystem.cs
+++ src/Chunkyard/Infrastructure/FileBlobSystem.cs
@@ -95,7 +95,7 @@ public sealed class FileBlobSystem : IBlobSystem
         }
         else
         {
-            throw new IOException($"Path does not exist: {path}");
+            return Array.Empty<string>();
         }
     }
 
