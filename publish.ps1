7f3786b92fa0e124bf185691ade13cdaed36ae4a
Florian Winkelbauer <mail@florianwinkelbauer.com>
2024-10-22 15:28:17 +0200

    Update exit condition in publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,8 @@
 try {
     Push-Location $PSScriptRoot
     dotnet run --no-launch-profile --project 'src/Publish/Publish.csproj' -- $args
+    if ($LASTEXITCODE -ne 0) { throw }
 }
 finally {
     Pop-Location
 }
-
-exit $LASTEXITCODE
