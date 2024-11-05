ca013ea72caa24135e242ca13afaa0d4128cbbc7
Florian Winkelbauer <mail@florianwinkelbauer.com>
2024-11-05 20:09:13 +0100

    Replace src/Publish/Publish.csproj with bash

--- publish.ps1
+++
@@ -1,8 +0,0 @@
-try {
-    Push-Location $PSScriptRoot
-    dotnet run --no-launch-profile --project 'src/Publish/Publish.csproj' -- $args
-    if ($LASTEXITCODE -ne 0) { throw }
-}
-finally {
-    Pop-Location
-}
--- src/Publish/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Diagnostics;
-global using System.IO;
-global using System.Linq;
-global using System.Text.RegularExpressions;
-
-[assembly: CLSCompliant(false)]
