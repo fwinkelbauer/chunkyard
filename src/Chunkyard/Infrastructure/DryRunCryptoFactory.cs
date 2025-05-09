25276b4464bf4a480c7d6392c5ccd3f8ae60a30e
Florian Winkelbauer <mail@florianwinkelbauer.com>
2025-05-09 13:12:47 +0200

    Add DryRunCryptoFactory

---
+++ src/Chunkyard/Infrastructure/DryRunCryptoFactory.cs
@@ -0,0 +1,30 @@
+namespace Chunkyard.Infrastructure;
+
+/// <summary>
+/// A <see cref="ICryptoFactory"/> decorator that generates a password for empty
+/// repositories.
+/// </summary>
+public class DryRunCryptoFactory : ICryptoFactory
+{
+    private readonly ICryptoFactory _cryptoFactory;
+
+    public DryRunCryptoFactory(ICryptoFactory cryptoFactory)
+    {
+        _cryptoFactory = cryptoFactory;
+    }
+
+    public Crypto Create(SnapshotReference? snapshotReference)
+    {
+        if (snapshotReference == null)
+        {
+            return new Crypto(
+                Guid.NewGuid().ToString(),
+                RandomNumberGenerator.GetBytes(Crypto.SaltBytes),
+                Crypto.DefaultIterations);
+        }
+        else
+        {
+            return _cryptoFactory.Create(snapshotReference);
+        }
+    }
+}
