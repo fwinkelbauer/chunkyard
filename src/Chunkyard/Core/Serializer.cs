85577165b47989e4fc3ff71ef20ebf832de1f73c
Florian Winkelbauer <mail@florianwinkelbauer.com>
2025-08-03 16:24:22 +0200

    Rename class

--- src/Chunkyard/Core/Serializer.cs
+++ src/Chunkyard/Core/Serializer.cs
@@ -5,7 +5,7 @@ namespace Chunkyard.Core;
 /// </summary>
 [JsonSerializable(typeof(Snapshot))]
 [JsonSerializable(typeof(SnapshotReference))]
-public sealed partial class Serialize : JsonSerializerContext
+public sealed partial class Serializer : JsonSerializerContext
 {
     public static byte[] SnapshotToBytes(Snapshot snapshot)
     {
