d8b35444b9598fe4f02323779ce8f860f193c6aa
Florian Winkelbauer <mail@florianwinkelbauer.com>
2025-08-08 20:52:45 +0200

    Add summary

--- src/Chunkyard/Core/ICryptoFactory.cs
+++ src/Chunkyard/Core/ICryptoFactory.cs
@@ -1,5 +1,9 @@
 namespace Chunkyard.Core;
 
+/// <summary>
+/// Create an instance of <see cref="Crypto"/> based on a
+/// <see cref="SnapshotReference"/>.
+/// </summary>
 public interface ICryptoFactory
 {
     Crypto Create(SnapshotReference? snapshotReference);
