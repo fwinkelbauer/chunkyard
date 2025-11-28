55e4c80f12df779192e4c168186e15fae616c1a8
Florian Winkelbauer <mail@florianwinkelbauer.com>
2025-11-28 13:12:32 +0100

    Migrate to new extension members

--- src/Chunkyard/Extensions.cs
+++ src/Chunkyard/Extensions.cs
@@ -3,158 +3,159 @@ namespace Chunkyard;
 /// <summary>
 /// A collection of extension methods.
 /// </summary>
-public static class Extensions
+internal static class Extensions
 {
-    public static bool CheckAll<T>(
-        this IEnumerable<T> elements,
-        Func<T, bool> checkFunc)
+    extension<T>(IEnumerable<T> elements)
     {
-        var results = elements.Select(e =>
+        public bool CheckAll(Func<T, bool> checkFunc)
         {
-            try
+            var results = elements.Select(e =>
             {
-                return checkFunc(e);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        });
+                try
+                {
+                    return checkFunc(e);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
 
-        return results.Aggregate(true, (total, next) => total && next);
+            return results.Aggregate(true, (total, next) => total && next);
+        }
     }
 
-    public static byte[] Retrieve<T>(this IRepository<T> repository, T key)
+    extension<T>(IRepository<T> repository)
     {
-        using var memoryStream = new MemoryStream();
-        using var valueStream = repository.OpenRead(key);
-
-        valueStream.CopyTo(memoryStream);
-
-        return memoryStream.ToArray();
-    }
-
-    public static IEnumerable<Blob> ListBlobs(
-        this IBlobSystem blobSystem,
-        Fuzzy fuzzy)
-    {
-        return blobSystem.ListBlobs()
-            .Where(b => fuzzy.IsMatch(b.Name));
-    }
-
-    public static Blob[] ListBlobs(
-        this Snapshot snapshot,
-        Fuzzy fuzzy)
-    {
-        return snapshot.BlobReferences
-            .Select(br => br.Blob)
-            .Where(b => fuzzy.IsMatch(b.Name))
-            .ToArray();
-    }
-
-    public static BlobReference[] ListBlobReferences(
-        this Snapshot snapshot,
-        Fuzzy fuzzy)
-    {
-        return snapshot.BlobReferences
-            .Where(b => fuzzy.IsMatch(b.Blob.Name))
-            .ToArray();
-    }
-
-    public static bool TrySnapshotStore(
-        this FlagConsumer consumer,
-        out SnapshotStore snapshotStore)
-    {
-        var success = consumer.TryRepository("--repository", "The repository path", out var repository)
-            & consumer.TryEnum("--password", "The password prompt method", out Password password, Password.Console);
-
-        ICryptoFactory cryptoFactory = password switch
+        public byte[] Retrieve(T key)
         {
-            Password.Console => new ConsoleCryptoFactory(),
-            Password.Libsecret => new LibsecretCryptoFactory(new ConsoleCryptoFactory()),
-            _ => new ConsoleCryptoFactory()
-        };
+            using var memoryStream = new MemoryStream();
+            using var valueStream = repository.OpenRead(key);
 
-        _ = consumer.TryDryRun(cryptoFactory, c => new DryRunCryptoFactory(c), out cryptoFactory);
+            valueStream.CopyTo(memoryStream);
 
-        snapshotStore = new SnapshotStore(
-            repository,
-            new ConsoleProbe(),
-            cryptoFactory);
-
-        return success;
+            return memoryStream.ToArray();
+        }
     }
 
-    public static bool TryRepository(
-        this FlagConsumer consumer,
-        string flag,
-        string info,
-        out IRepository repository)
+    extension(IBlobSystem blobSystem)
     {
-        var success = consumer.TryString(flag, info, out var path)
-            & consumer.TryDryRun(new FileRepository(path), r => new DryRunRepository(r), out repository);
-
-        return success;
+        public IEnumerable<Blob> ListBlobs(Fuzzy fuzzy)
+        {
+            return blobSystem.ListBlobs()
+                .Where(b => fuzzy.IsMatch(b.Name));
+        }
     }
 
-    public static bool TryBlobSystem(
-        this FlagConsumer consumer,
-        string flag,
-        string info,
-        out IBlobSystem blobSystem)
+    extension(Snapshot snapshot)
     {
-        var success = consumer.TryStrings(flag, info, out var directories)
-            & consumer.TryDryRun(new FileBlobSystem(directories), b => new DryRunBlobSystem(b), out blobSystem);
+        public Blob[] ListBlobs(Fuzzy fuzzy)
+        {
+            return snapshot.BlobReferences
+                .Select(br => br.Blob)
+                .Where(b => fuzzy.IsMatch(b.Name))
+                .ToArray();
+        }
 
-        return success;
+        public BlobReference[] ListBlobReferences(Fuzzy fuzzy)
+        {
+            return snapshot.BlobReferences
+                .Where(b => fuzzy.IsMatch(b.Blob.Name))
+                .ToArray();
+        }
     }
 
-    public static bool TrySnapshot(
-        this FlagConsumer consumer,
-        out int snapshot)
+    extension(FlagConsumer consumer)
     {
-        return consumer.TryInt(
-            "--snapshot",
-            "The snapshot ID",
-            out snapshot,
-            SnapshotStore.LatestSnapshotId);
-    }
+        public bool TrySnapshotStore(out SnapshotStore snapshotStore)
+        {
+            var success = consumer.TryRepository("--repository", "The repository path", out var repository)
+                & consumer.TryEnum("--password", "The password prompt method", out Password password, Password.Console);
 
-    public static bool TryInclude(
-        this FlagConsumer consumer,
-        out Fuzzy include)
-    {
-        var success = consumer.TryStrings(
-            "--include",
-            "A list of fuzzy patterns for files to include",
-            out var includePatterns,
-            Array.Empty<string>());
+            ICryptoFactory cryptoFactory = password switch
+            {
+                Password.Console => new ConsoleCryptoFactory(),
+                Password.Libsecret => new LibsecretCryptoFactory(new ConsoleCryptoFactory()),
+                _ => new ConsoleCryptoFactory()
+            };
 
-        include = success
-            ? new Fuzzy(includePatterns)
-            : new Fuzzy();
+            _ = consumer.TryDryRun(cryptoFactory, c => new DryRunCryptoFactory(c), out cryptoFactory);
 
-        return success;
-    }
+            snapshotStore = new SnapshotStore(
+                repository,
+                new ConsoleProbe(),
+                cryptoFactory);
 
-    public static bool TryDryRun<T>(
-        this FlagConsumer consumer,
-        T input,
-        Func<T, T> decorator,
-        out T output)
-    {
-        var success = consumer.TryBool(
-            "--dry-run",
-            "Do not persist any data changes",
-            out var dryRun);
+            return success;
+        }
 
-        output = dryRun ? decorator(input) : input;
+        public bool TryRepository(
+            string flag,
+            string info,
+            out IRepository repository)
+        {
+            var success = consumer.TryString(flag, info, out var path)
+                & consumer.TryDryRun(new FileRepository(path), r => new DryRunRepository(r), out repository);
 
-        return success;
+            return success;
+        }
+
+        public bool TryBlobSystem(
+            string flag,
+            string info,
+            out IBlobSystem blobSystem)
+        {
+            var success = consumer.TryStrings(flag, info, out var directories)
+                & consumer.TryDryRun(new FileBlobSystem(directories), b => new DryRunBlobSystem(b), out blobSystem);
+
+            return success;
+        }
+
+        public bool TrySnapshot(out int snapshot)
+        {
+            return consumer.TryInt(
+                "--snapshot",
+                "The snapshot ID",
+                out snapshot,
+                SnapshotStore.LatestSnapshotId);
+        }
+
+        public bool TryInclude(out Fuzzy include)
+        {
+            var success = consumer.TryStrings(
+                "--include",
+                "A list of fuzzy patterns for files to include",
+                out var includePatterns,
+                Array.Empty<string>());
+
+            include = success
+                ? new Fuzzy(includePatterns)
+                : new Fuzzy();
+
+            return success;
+        }
+
+        public bool TryDryRun<T>(
+            T input,
+            Func<T, T> decorator,
+            out T output)
+        {
+            var success = consumer.TryBool(
+                "--dry-run",
+                "Do not persist any data changes",
+                out var dryRun);
+
+            output = dryRun ? decorator(input) : input;
+
+            return success;
+        }
     }
 }
 
-public enum Password
+/// <summary>
+/// Describes password retrieval mechanisms.
+/// </summary>
+internal enum Password
 {
     Console = 0,
     Libsecret = 1
